22c06351f8368eb3246a5a2af0c1de781b546510
Alyan-Asif12 <girishourya569@gmail.com>
2024-08-06 18:39:14 +0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav ul li a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const targetId =
