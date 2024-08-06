6d9c380b4a4dd24a5f734b285fa56b5c927a19fc
Alyan-Asif12 <girishourya569@gmail.com>
2024-08-06 17:00:40 +0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,4 @@ document.addEventListener('DOMContentLoaded', function() {
     navLinks.forEach(link => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            const targetId = e.target.getAttribute('href').substring(1);
-            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-        });
-    });
-});
+            const targetId =
