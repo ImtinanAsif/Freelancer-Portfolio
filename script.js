f2e1e7864307d5180603abfff605b824597005e4
Imtinan <girishourya569@gmail.com>
2026-04-28 07:54:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     typeText();
 });
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+}
